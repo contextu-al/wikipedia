8302f1e0fef744eeacccbdc17cc7a9ddcc8b132f
Daniel Quah <daniel@contextu.al>
2023-02-17 15:33:49 +1100

    Update build process
    - Update Android SDK version to 33
    - Use gradle flavour to build different version of the sdk

--- build.sh
+++ build.sh
@@ -21,27 +21,27 @@ SDK_ENV="Dev"
 GIT_VERSION=$(git log -1 --format="%h")
 BUILD_TIME=$(date)
 
+echo "===== Version of Contextual SDK installed ====="
+./gradlew app:dependencies | grep pointzi
+
 # Default is Develop using above environment variables
 # Staging
+echo "===== Build NewPipe .apk for AppCenter ====="
 if [ "$GIT_BRANCH" = "staging" ]; then 
   APP_ENV="Staging"
   APP_KEY="Wikipedia_staging"
+  ./gradlew assembleStaging
 # Production
 elif [ "$GIT_BRANCH" = "main" ]; then
   SDK_ENV='Prod'
-  # Update the build.gradle to point to the prod version of the SDK
+  ./gradlew assembleRelease
 fi
 
 # We use lowercase variables as part of the Artifactory BDD path below
 LOWERCASE_APP_ENV=$( tr '[A-Z]' '[a-z]' <<< $APP_ENV)
 LOWERCASE_SDK_ENV=$( tr '[A-Z]' '[a-z]' <<< $SDK_ENV)
 
-echo "===== Build Wikipedia .apk for AppCenter ====="
-./gradlew assembleDebug
-
-echo "===== Version of Contextual SDK installed ====="
-./gradlew app:dependencies | grep pointzi
 
 
 echo "===== Uploading .apk to AppCenter ====="
-appcenter distribute release --app Contextual/Wikipedia-"$SDK_ENV"SDK-"$APP_ENV"-"$APP_KEY"-Android --file "app/build/outputs/apk/debug/app-debug.apk" --group "Collaborators"
+appcenter distribute release --app Contextual/Wikipedia-"$SDK_ENV"SDK-"$APP_ENV"-"$APP_KEY"-Android --file "app/build/outputs/apk/release/app-release.apk" --group "Collaborators"
